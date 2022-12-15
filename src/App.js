54e1626142e54ddf5e8fc28fa70b3098cb0682e3
Csfahad <76874725+Csfahad@users.noreply.github.com>
2022-12-15 18:24:51 +0530

    Final build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PreNavbar from "./components/PreNavbar";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+// import {banner} from './data/data'
+import data from "./data/data.json";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Offers from "./components/Offers";
+import Heading from "./components/Heading";
+import StarProduct from "./components/StarProduct";
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu";
+import HotAccessories from "./components/HotAccessories";
+import ProductReviews from "./components/ProductReviews";
+import Videos from "./components/Videos";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import NavOptions from "./components/NavOptions";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <PreNavbar />
+      <Navbar />
+      <NavOptions
+        miPhones={data.miPhones}
+        redmiPhones={data.redmiPhones}
+        tv={data.tv}
+        laptop={data.laptop}
+        fitnessAndLifeStyle={data.fitnessAndLifeStyle}
+        home={data.home}
+        audio={data.audio}
+        accessories={data.accessories}
+      />
+      <Slider start={data.banner.start} />
+      <Offers offer={data.offer} />
+      <Heading text="STAR PRODUCTS" />
+      <StarProduct starProduct={data.starProduct} />
+      <Heading text="HOT ACCESSORIES" />
+      <HotAccessoriesMenu />
+      <Routes>
+        <Route
+          path="/music"
+          element={
+            <HotAccessories
+              music={data.hotAccessories.music}
+              musicCover={data.hotAccessoriesCover.music}
+            />
+          }
+        />
+        <Route
+          path="/smartDevice"
+          element={
+            <HotAccessories
+              smartDevice={data.hotAccessories.smartDevice}
+              smartDeviceCover={data.hotAccessoriesCover.smartDevice}
+            />
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <HotAccessories
+              home={data.hotAccessories.home}
+              homeCover={data.hotAccessoriesCover.home}
+            />
+          }
+        />
+        <Route
+          path="/lifestyle"
+          element={
+            <HotAccessories
+              lifeStyle={data.hotAccessories.lifeStyle}
+              lifeStyleCover={data.hotAccessoriesCover.lifeStyle}
+            />
+          }
+        />
+        <Route
+          path="/mobileAccessories"
+          element={
+            <HotAccessories
+              mobileAccessories={data.hotAccessories.mobileAccessories}
+              mobileAccessoriesCover={
+                data.hotAccessoriesCover.mobileAccessories
+              }
+            />
+          }
+        />
+      </Routes>
+      <Heading text="PRODUCT REVIEWS" />
+      <ProductReviews productReviews={data.productReviews} />
+      <Heading text="VIDEOS" />
+      <Videos videos={data.videos} />
+      <Heading text="IN THE PRESS" />
+      <Banner banner={data.banner} />
+      <Footer footer={data.footer} />
+    </BrowserRouter>
   );
 }
 
